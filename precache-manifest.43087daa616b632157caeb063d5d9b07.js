3aa50994843b3c2c86fa8b5f5c88e0e5716f0fb4
Bogdan Odintsov <bacha1122001@gmail.com>
2022-07-14 04:02:09 +0300

    Updates

---
+++ precache-manifest.43087daa616b632157caeb063d5d9b07.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90631a64450b5d4fd2c7fd3bbabc0179",
+    "url": "/odintsovPizza/index.html"
+  },
+  {
+    "revision": "1316a9dca1e5017b704b",
+    "url": "/odintsovPizza/static/css/main.7cbe9168.chunk.css"
+  },
+  {
+    "revision": "420d1e81b982c36e1544",
+    "url": "/odintsovPizza/static/js/2.ea72b929.chunk.js"
+  },
+  {
+    "revision": "4e4d7974b6057c359a2979db6a227004",
+    "url": "/odintsovPizza/static/js/2.ea72b929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1316a9dca1e5017b704b",
+    "url": "/odintsovPizza/static/js/main.9011ebe2.chunk.js"
+  },
+  {
+    "revision": "af7e03032dfda8314371",
+    "url": "/odintsovPizza/static/js/runtime-main.d0c4636c.js"
+  },
+  {
+    "revision": "4325ed848b66ed370420ea7fea09bc80",
+    "url": "/odintsovPizza/static/media/ProximaNova-Black.4325ed84.eot"
+  },
+  {
+    "revision": "a0606c5fef28b9e57f501db027063141",
+    "url": "/odintsovPizza/static/media/ProximaNova-Black.a0606c5f.ttf"
+  },
+  {
+    "revision": "b978d1b46a86df7a012cf8442936a1cb",
+    "url": "/odintsovPizza/static/media/ProximaNova-Black.b978d1b4.woff"
+  },
+  {
+    "revision": "10141a9d737da9c84e6e14ec5a562c6c",
+    "url": "/odintsovPizza/static/media/ProximaNova-Bold.10141a9d.eot"
+  },
+  {
+    "revision": "926a08fb27e3303c7452b0bdd2d5e5ab",
+    "url": "/odintsovPizza/static/media/ProximaNova-Bold.926a08fb.ttf"
+  },
+  {
+    "revision": "e2cf3dc2f079bf3d5185a02552f153c4",
+    "url": "/odintsovPizza/static/media/ProximaNova-Bold.e2cf3dc2.woff"
+  },
+  {
+    "revision": "169bc0f1eb2d8d0d8678014d368b0226",
+    "url": "/odintsovPizza/static/media/ProximaNova-Extrabld.169bc0f1.eot"
+  },
+  {
+    "revision": "51563919fd1af7376fac14b1194a8e4e",
+    "url": "/odintsovPizza/static/media/ProximaNova-Extrabld.51563919.ttf"
+  },
+  {
+    "revision": "ccf59c77109aecc8deeeede82e3bac87",
+    "url": "/odintsovPizza/static/media/ProximaNova-Extrabld.ccf59c77.woff"
+  },
+  {
+    "revision": "2d2ae2556b24a45ff8d5ed86b07b5783",
+    "url": "/odintsovPizza/static/media/ProximaNova-Regular.2d2ae255.woff"
+  },
+  {
+    "revision": "7ce6760d17685c466ba04d1b2c63c38b",
+    "url": "/odintsovPizza/static/media/ProximaNova-Regular.7ce6760d.ttf"
+  },
+  {
+    "revision": "b7c512788e3c77b0196f0bace8a88418",
+    "url": "/odintsovPizza/static/media/ProximaNova-Regular.b7c51278.eot"
+  },
+  {
+    "revision": "f3327011e80a4caf08837bd844e21996",
+    "url": "/odintsovPizza/static/media/pizza-logo.f3327011.svg"
+  }
+]);
